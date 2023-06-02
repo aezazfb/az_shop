dca064ebb15e45d792e33565ffe79f83dbbbef9d
az <aezazfb@hotmail.com>
2023-06-02 12:05:18 +0500

    Getting data from firebase stream null error resolved

--- lib/app_pages/HomePageGetX.dart
+++ lib/app_pages/HomePageGetX.dart
@@ -22,83 +22,91 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: ListView(
-        children: [
-          StreamBuilder(
-              stream: _red.snapshots(),
-              builder: (BuildContext stContext, AsyncSnapshot<QuerySnapshot> theSnap){
-                productVar = theSnap.data?.docs.toList();
-                num x= theSnap.data?.docs.length as num;
-                return Column(
-                  children: [
-                    for(int indx =0; indx <= x -1; indx ++)
-                    Column(
-                      children: [
-                        Text(productVar[indx]["expiry"].toString()),
-                        Text(productVar[indx]["name"].toString()),
-                        Text(productVar[indx]["quantity"].toString()),
-                      ],
-                    ),
-                  ],
-                );
-              }),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(children: [
-              for(int i = 1; i<=2; i++)
-                Image.asset("myassets/cover$i.png",
-                  height: 250,
-                  fit: BoxFit.cover,),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            StreamBuilder(
+                stream: _red.snapshots(),
+                builder: (BuildContext stContext, AsyncSnapshot<QuerySnapshot> theSnap){
+                  productVar = theSnap.data?.docs.toList();
+                  num x = 0;
+                  if(theSnap.hasData){
+                    x= theSnap.data?.docs.length as num;
+                  }
 
-            ]),
-          ),
 
-          const SizedBox(height: 20,),
+                  return theSnap.hasData ? Column(
+                    children: [
+                      for(int indx =0; indx <= x -1; indx ++)
+                      Column(
+                        children: [
+                          Text(productVar[indx]["expiry"].toString()),
+                          Text(productVar[indx]["name"].toString()),
+                          Text(productVar[indx]["quantity"].toString()),
+                        ],
+                      ),
+                    ],
+                  ) :  CircularProgressIndicator();
+                }),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(children: [
+                for(int i = 1; i<=2; i++)
+                  Image.asset("myassets/cover$i.png",
+                    height: 250,
+                    fit: BoxFit.cover,),
 
-          Container(
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                )
-              ],
+              ]),
             ),
-            child: GridView.count(
-              crossAxisCount: 4,
-              childAspectRatio: 1,
-              crossAxisSpacing: 15,
-              mainAxisSpacing: 15,
-              shrinkWrap: true,
-              children: [
-                for(int x = 1; x<=5; x++)
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 230, 177),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                        )
-                      ],
 
-                    ),
-                    child: Image.asset("myassets/$x.png"),
+            const SizedBox(height: 20,),
+
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 5,
                   )
-              ],
+                ],
+              ),
+              child: GridView.count(
+                crossAxisCount: 4,
+                childAspectRatio: 1,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                shrinkWrap: true,
+                children: [
+                  for(int x = 1; x<=5; x++)
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 230, 177),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                          )
+                        ],
+
+                      ),
+                      child: Image.asset("myassets/$x.png"),
+                    )
+                ],
+              ),
             ),
-          ),
 
-          const HomePagePosts(),
+            const HomePagePosts(),
 
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomBar(),
     );
